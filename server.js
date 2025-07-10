ce37d815b46e853ec8b5f676a745a3858537ba8c
Caroline J. Adesina <caroline.adesina@lbsl.ng>
2025-07-11 00:57:06 +0100

    updated

--- server.js
+++ server.js
@@ -22,12 +22,12 @@ const server = http.createServer(async (req, res) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
 
     if (req.method === 'OPTIONS') {
-    res.writeHead(204, {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE',
-      'Access-Control-Allow-Headers': 'Content-Type'
-    });
-    return res.end();
+      res.writeHead(204, {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE',
+        'Access-Control-Allow-Headers': 'Content-Type'
+      });
+        res.end();
     }
 
   const db = getDB()
